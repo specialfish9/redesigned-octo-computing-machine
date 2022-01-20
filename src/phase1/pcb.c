08dfa713e076a9dd546c550207256958029c30db
Mattia <mgiro2001@gmail.com>
2022-01-20 20:22:34 +0100

    working on trees functions

--- src/phase1/pcb.c
+++ src/phase1/pcb.c
@@ -1,17 +1,45 @@
 #include "pcb.h"
 #include "listx.h"
+#include "pandos_types.h"
 
 /*
- *Inizializza la lista pcbFree in modo da contenere tutti gli elementi della pcbFree_table. 
+ *Inizializza la lista pcbFree in modo da contenere tutti gli elementi della
+ *pcbFree_table.
  */
-void initPcbs(void){
-	pcbFree_h->p_list =LIST_HEAD_INIT(pcbFree_h->p_list);
+void initPcbs(void)
+{
+  pcbFree_h->p_list = LIST_HEAD_INIT(pcbFree_h->p_list);
 
-	for(int i = 0; i < MAX_PROC; i++) {
-	  list_add(pcbFree_table[i], pcbFree_h->p_list);
-	}
-	
-	for (pcb_t* ptr = pcbFree_table; ptr != NULL; ptr ++){
-	  list_add(ptr, pcbFree_h->p_list);
-	}
+  for (int i = 0; i < MAXPROC; i++) {
+    list_add(pcbFree_table[i], pcbFree_h->p_list);
+  }
+
+  for (pcb_t *ptr = pcbFree_table; ptr != NULL; ptr++) {
+    list_add(ptr, pcbFree_h->p_list);
+  }
+}
+
+const int emptyChild(const pcb_t *p) { return list_empty(&p->p_child); }
+
+void insertChild(pcb_t *prnt, pcb_t *p)
+{
+  list_add(&p->p_list, &prnt->p_child);
+}
+
+pcb_t *removeChild(pcb_t *p)
+{
+  if (list_empty(&p->p_child)) {
+    return NULL;
+  }
+
+  list_del(&p->p_child);
+  return p;
+}
+
+pcb_t *outChild(pcb_t *p)
+{
+  pcb_t *father = p->p_parent;
+
+  if (p == NULL)
+    return NULL;
 }
--- src/phase1/pcb.h
+++ src/phase1/pcb.h
@@ -1,11 +1,33 @@
 #ifndef PCB_H
 #define PCB_H
-#include "pandos_types.h"
 #include "pandos_const.h"
+#include "pandos_types.h"
 
-static pcb_t pcbFree_table[MAX_PROC]; 
+static pcb_t pcbFree_table[MAXPROC];
 static pcb_t *pcbFree_h;
 
+/* LIST FUNCTIONS */
+
 void initPcbs(void);
+
+/* TREES FUNCTIONS */
+
+/* Returns TRUE if the PCB pointed by p hasn't got any child. FALSE otherwise */
+const int emptyChild(const pcb_t *p);
+
+/* Inserts the PCB pointed by p as a child of the PCB pointed by prnt */
+void insertChild(pcb_t *prnt, pcb_t *p);
+
+/* Removes the first child of the PCB pointed by p. If p hasn't children
+ * returns NULL*/
+pcb_t *removeChild(pcb_t *p);
+
+/*
+ * Removes the PCB pointed by p from the list of children of his father.
+ * If the PCB pointed by p hasn't a father then returns NULL, otherwise returns
+ * the deleted element (p). p can be in every position, it may not be the first
+ * child .
+ */
+pcb_t *outChild(pcb_t *p);
 
 #endif
