5d7ee084208d58249931d745d6cc69947b106abf
andreeascrob <andreeascrob@icloud.com>
2022-01-23 16:39:09 +0100

    list head init error

--- src/phase1/pcb.c
+++ src/phase1/pcb.c
@@ -1,33 +1,46 @@
 #include "pcb.h"
 #include "listx.h"
 
+static pcb_t pcbFree_table[MAXPROC];
+
 /*
- *Inizializza la lista pcbFree in modo da contenere tutti gli elementi della pcbFree_table. 
+ *Inizializza la lista pcbFree in modo da contenere tutti gli elementi della
+ *pcbFree_table.
  */
-void initPcbs(void){
-	pcbFree_h=LIST_HEAD_INIT(pcbFree_h);
+void initPcbs(void)
+{
+  struct list_head pcbFree_h;
 
-	for(int i = 0; i < MAX_PROC; i++) {
-		  list_add( &pcbFree_table[i].p_list, &pcbFree_h);
-	}
-	
-/*	for (pcb_t* ptr = pcbFree_table; ptr != NULL; ptr ++){
-	  list_add(ptr, pcbFree_h->p_list);
-	}*/
-}
-void freePcb(pcb_t *p){
-	list_del(&p->p_list);
-	list_add(&p->p_list, &pcbFree_h);
+  pcbFree_h = LIST_HEAD_INIT(pcbFree_h);
+
+  for (size_tt i = 0; i < MAXPROC; i++)
+    list_add_tail(pcbFree_table[i].p_list, pcbFree_h.p_list);
 }
 
-pcb_t *allocPcb(){
-	if(list_empty(&pcbFree_h))
-		return NULL;
-	pcb_t *pcb= container_of(pcbFree_h.next, pcb_t, p_list);
-	list_del(pcbFree_h.next);
-	pcb->p_parent=NULL;
-	pcb->p_s=0;
-	pcb->p_time=0;
-	pcb->p_semAdd=NULL;
-	return pcb;
+void freePcb(pcb_t *p)
+{
+  /*list_del(&p->p_list);*/
+  list_add(p->p_list, pcbFree_h.p_list);
 }
+
+pcb_t *allocPcb()
+{
+  if (list_empty(pcbFree_h.p_list))
+    return NULL;
+  pcb_t *pcb = container_of(pcbFree_h.p_list->next, pcb_t, p_list);
+  list_del(pcbFree_h.p_list->next);
+  pcb->p_parent = NULL;
+  pcb->p_s = 0;
+  pcb->p_time = 0;
+  pcb->p_semAdd = NULL;
+  return pcb;
+}
+
+/*void mkEmptyProcQ(struct list_head *head){
+
+}
+
+int emptyProcQ (struct list_head *head){
+
+}
+*/
--- src/phase1/pcb.h
+++ src/phase1/pcb.h
@@ -1,11 +1,10 @@
 #ifndef PCB_H
 #define PCB_H
-#include "pandos_types.h"
 #include "pandos_const.h"
-
-static pcb_t pcbFree_table[MAX_PROC]; 
-static struct  list_head pcbFree_h;
+#include "pandos_types.h"
 
 void initPcbs(void);
+void freePcb(pcb_t *p);
+pcb_t *allocPcb();
 
 #endif
