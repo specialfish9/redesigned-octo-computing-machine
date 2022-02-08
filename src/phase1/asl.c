40d4b355d6c5ee814418b88808c0a860bf8eeea1
Gabriele Crestanello <gabricre01@gmail.com>
2022-02-08 16:11:13 +0100

    insertBlocked + removeBlocked

--- src/phase1/asl.c
+++ src/phase1/asl.c
@@ -23,11 +23,38 @@ semd_t* getSemd(int *s_key){    //per ottenere il semaforo di appartenenza a par
 int insertBlocked(int *semAdd,pcb_t *p){    //p punta al PCB dalla coda dei processi bloccati, semAdd è la chiave per il SEMD associato a quella coda. Return TRUE se non è possibile allocare un SEMD perchè la lista dei liberi è vuota, FALSE altrimenti
     struct semd_t *s = getSemd(p->p_semAdd);   //ottengo il semaforo su cui è bloccato p
 
+    if(s == NULL){  //se il semaforo corrispondente a semAdd non esiste nella ASL
+        if(list_empty(semdFree_h))
+            return TRUE;    //restituisco TRUE se non è possibile allocare un nuovo semaforo perchè la lista di quelli liberi è vuota
+        else{
+            struct semd_t *tmp = container_of(semdFree_h->next,semd_t,s_link);      //ottengo un nuovo SEMD dalla lista dei liberi
+            list_del(semdFree_h->next);             //elimino il SEMD dalla lista dei liberi
+            list_add(&(tmp->s_link),semd_h);        //lo inserisco nella lista dei SEMD
+
+            insertProcQ(&(tmp->s_procq),p);         //associo il processo al SEMD
+            tmp->s_key = semAdd;
+        }
+    }else       //se il semaforo esiste già inserisco il processo in coda
+        insertProcQ(&(s->s_procq),p);
+    
+    p->p_semAdd = semAdd;
+
+    return FALSE; //restituisco FALSE come caso base
 }
 
 pcb_t* removeBlocked(int *semAdd){  //rimuovere il primo PCB nella lista dei bloccati associati al SEMD tramite semAdd. Return del PCB, altrimenti null se non esiste
     struct semd_t *s = getSemd(semAdd);   //ottengo il semaforo associato a semAdd
+//FORSE QUESTO IF E' SUPERFLUO
+    if(s == NULL)
+        return NULL;
 
+    struct pcb_t *pcb = removeProcQ(&(s->s_procq));  //rimuovo il primo elemento bloccato su quel SEMD
+    
+    if(list_empty(&(s->s_procq))){              //Se la coda dei processi bloccati per il semaforo diventa vuota
+        list_del(&(s->s_link));                 //rimuove il descrittore corrispondente dalla ASL
+        list_add(&(s->s_link),semdFree_h);      //e lo inserisce nella coda dei descrittori liberi
+    }
+    return pcb;       //return del PCB se compare
 }
 
 pcb_t* outBlocked(pcb_t *p){    //rimuovere PCB puntato da p->p_semAdd dalla coda del semaforo su cui è bloccato. Return null se il PCB non compare, p altrimenti
@@ -42,7 +69,7 @@ pcb_t* outBlocked(pcb_t *p){    //rimuovere PCB puntato da p->p_semAdd dalla cod
         list_del(&(s->s_link));                 //rimuove il descrittore corrispondente dalla ASL
         list_add(&(s->s_link),semdFree_h);      //e lo inserisce nella coda dei descrittori liberi
     }
-    return pcb;       //return p se il PCB compare
+    return pcb;       //return del PCB se compare
 
 }
 
