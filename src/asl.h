81dc2ef8bddd1e0fc8ab5806776ad31b5b4bf8f4
mattia <mattia.porco981@gmail.com>
2022-02-10 23:29:56 +0100

    clean project structure

--- src/asl.h
+++ src/asl.h
@@ -3,11 +3,9 @@
 
 #include "pandos_types.h"
 
-
-
 /*-----ACTIVE SEMAPHORE LIST FUNCTIONS-----*/
 
-extern int insertBlocked(int *semAdd,pcb_t *p);
+extern int insertBlocked(int *semAdd, pcb_t *p);
 /*Viene inserito il PCB puntato da p nella coda dei
 processi bloccati associata al SEMD con chiave
 semAdd. Se il semaforo corrispondente non è
@@ -19,8 +17,7 @@ nuovo SEMD perché la lista di quelli liberi è vuota,
 restituisce TRUE. In tutti gli altri casi, restituisce
 FALSE.*/
 
-
-extern pcb_t* removeBlocked(int *semAdd);
+extern pcb_t *removeBlocked(int *semAdd);
 /*Ritorna il primo PCB dalla coda dei processi
 bloccati (s_procq) associata al SEMD della
 ASL con chiave semAdd. Se tale descrittore
@@ -29,10 +26,9 @@ Altrimenti, restituisce l’elemento rimosso. Se
 la coda dei processi bloccati per il semaforo
 diventa vuota, rimuove il descrittore
 corrispondente dalla ASL e lo inserisce nella
-coda dei descrittori liberi (semdFree_h).*/     
+coda dei descrittori liberi (semdFree_h).*/
 
-
-extern pcb_t* outBlocked(pcb_t *p);
+extern pcb_t *outBlocked(pcb_t *p);
 /*Rimuove il PCB puntato da p dalla coda del semaforo
 su cui è bloccato (indicato da p->p_semAdd). Se il PCB
 non compare in tale coda, allora restituisce NULL
@@ -41,20 +37,18 @@ coda dei processi bloccati per il semaforo diventa
 vuota, rimuove il descrittore corrispondente dalla ASL
 e lo inserisce nella coda dei descrittori liberi*/
 
-
-extern pcb_t* headBlocked(int *semAdd);
+extern pcb_t *headBlocked(int *semAdd);
 /*Restituisce (senza rimuovere) il puntatore al PCB che
 si trova in testa alla coda dei processi associata al
 SEMD con chiave semAdd. Ritorna NULL se il SEMD
 non compare nella ASL oppure se compare ma la sua
 coda dei processi è vuota.*/
 
-
 extern void initASL(void);
 /*Inizializza la lista dei semdFree in
 modo da contenere tutti gli elementi
 della semdTable. Questo metodo
 viene invocato una volta sola durante
-l’inizializzazione della struttura dati.*/                          
+l’inizializzazione della struttura dati.*/
 
 #endif
