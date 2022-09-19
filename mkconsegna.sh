d57976ddff07e4a41efb76914fa25b3fee3eb882
mattia girolimetto <33939215+specialfish9@users.noreply.github.com>
2022-09-19 09:29:17 +0200

    Last commit!

--- mkconsegna.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-make all
-make clean
-
-tar -czf really-optimistic-computing-machine.tar.gz  .clang-format AUTHORS Doxyfile Makefile README.md assets/ config/ doc/ obj/ out/ output/ src/ test/
